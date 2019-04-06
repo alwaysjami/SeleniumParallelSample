27e3d3c849b4b94ab1d03302de5de7abfc74fffc
alwaysjami <46101702+alwaysjami@users.noreply.github.com>
2019-04-06 09:49:18 +0530

    Update Hooks.cs

--- SeleniumParallelTest/Hooks.cs
+++ SeleniumParallelTest/Hooks.cs
@@ -20,33 +20,34 @@ namespace SeleniumParallelTest
     {
         private BrowerType _browserType;
 
-
-        public Hooks(BrowerType browser)
-        {
-            _browserType = browser;
-        }
-
-
         [SetUp]
         public void InitializeTest()
         {
+            //Get the value from NUnit-console --params 
+            //e.g. nunit3-console.exe --params:Browser=Firefox \SeleniumNUnitParam.dll
+            //If nothing specified, test will run in Chrome browser
+            var browserType = TestContext.Parameters.Get("Browser", "Chrome");
+            //Parse the browser Type, since its Enum
+            _browserType = (BrowerType)Enum.Parse(typeof(BrowerType), browserType);
+            //Pass it to browser
             ChooseDriverInstance(_browserType);
         }
 
         private void ChooseDriverInstance(BrowerType browserType)
         {
-            //var driverDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembl‌​y().Location);
-
             if (browserType == BrowerType.Chrome)
                 Driver = new ChromeDriver();
             else if (browserType == BrowerType.Firefox)
             {
-                //FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(driverDir, "geckodriver.exe");
-                //service.FirefoxBinaryPath = @"C:\Program Files (x86)\Mozilla Firefox\firefox.exe";
-                //service.HideCommandPromptWindow = true;
-                //service.SuppressInitialDiagnosticInformation = true;
-                //Driver = new FirefoxDriver(service);
-                Driver = new ChromeDriver();
+                FirefoxDriverService service = FirefoxDriverService.CreateDefaultService();
+                service.FirefoxBinaryPath = @"C:\Program Files (x86)\Mozilla Firefox\firefox.exe";
+                service.HideCommandPromptWindow = true;
+                service.SuppressInitialDiagnosticInformation = true;
+                Driver = new FirefoxDriver(service);
+            }
+            else if(browserType == BrowerType.IE)
+            {
+                Driver = new InternetExplorerDriver();
             }
         }
 
@@ -55,7 +56,5 @@ namespace SeleniumParallelTest
         {
             Driver.Quit();
         }
-
-
     }
 }
